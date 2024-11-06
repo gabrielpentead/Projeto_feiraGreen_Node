dcec5f4b9f229da54f2210b4c3a2643abe947b88
gabrielpentead <gabrielpentead@gmail.com>
2024-11-06 15:08:50 -0300

    first commit

--- src/pages/SearchResults/index.js
+++ src/pages/SearchResults/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom'; // Importa o useLocation para capturar mudanças na URL
 import { data } from '../../data'; // Importa os dados
 import ProductList from '../Home/ProductList'; // Importa a lista de produtos
 
 function SearchResults() {
-  const searchQuery = window.location.pathname.split('/').pop(); // pega parte final do caminho URL assumindo que é o termo de busca
+  const [searchQuery, setSearchQuery] = useState('');
+  const location = useLocation(); // Captura a localização atual
+
+  // Atualiza o searchQuery sempre que a URL mudar
+  useEffect(() => {
+    const query = location.pathname.split('/').pop(); // Pega a parte final do caminho da URL
+    setSearchQuery(query); // Atualiza o estado com o novo termo de busca
+  }, [location]);
 
   // Filtra produtos com base no nome
-  const filteredData = data.filter((produto) => 
+  const filteredData = data.filter((produto) =>
     produto.name.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
